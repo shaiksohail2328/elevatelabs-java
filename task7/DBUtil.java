4573c29d64917abbec55714d424d76fa731f23b4
shaiksohail2328 <23r01a05q7@cmrithyderabad.edu.in>
2025-10-03 15:53:51 +0530

    Create DBUtil.java

---
+++ task7/DBUtil.java
@@ -0,0 +1,15 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/employeedb"; // or jdbc:postgresql://...
+    private static final String USER = "root";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
